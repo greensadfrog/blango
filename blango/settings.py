8b4e46515d07d48b02651437feb6793c5f53cb17
Mykhailo Dudka <m.dudka.iv92@kpi.ua>
2022-08-20 14:08:03 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -91,6 +91,10 @@ class Dev(Configuration):
 
   AUTH_USER_MODEL = "blango_auth.User"
 
+  EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+  ACCOUNT_ACTIVATION_DAYS = 7
+
 
   # Database
   # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
